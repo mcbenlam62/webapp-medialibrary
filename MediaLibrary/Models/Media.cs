49273511d7f792434c8c1e0e5c9485108024f191
Ben Lam <ben_lam@hotmail.com>
2016-04-19 13:52:56 +0800

    First release

---
+++ MediaLibrary/Models/Media.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace MediaLibrary.Models
+{
+    public class Media
+    {
+        public int MediaId { get; set; }
+        public string Media_Name { get; set; }
+        public string Streaming_Locator_URL { get; set; }
+        public string ThumbNail { get; set; }
+    }
+}
---
+++ MediaLibrary/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(MediaLibrary.Startup))]
+namespace MediaLibrary
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
